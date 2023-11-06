8f4627cbaac17ea8cd263404c0b74a0cb75ecbd1
Mohamed Mursith <mmursith@virtusa.com>
2023-11-06 09:35:07 +0530

    Update packages and code to cater keycloak 22

--- src/directives/schemaDirectiveVisitors.ts
+++ src/directives/schemaDirectiveVisitors.ts
@@ -1,8 +1,6 @@
 import { defaultFieldResolver, GraphQLSchema } from 'graphql';
-import {
-  SchemaDirectiveVisitor,
-  VisitableSchemaType,
-} from '@graphql-tools/utils';
+import { mapSchema, getDirective, MapperKind } from '@graphql-tools/utils';
+
 import {
   auth,
   authKey,
@@ -11,137 +9,116 @@ import {
   tenant,
 } from './directiveResolvers';
 
-export class AuthDirective extends SchemaDirectiveVisitor {
-  constructor(config: {
-    name: string;
-    visitedType: VisitableSchemaType;
-    schema: GraphQLSchema;
-    args: {};
-    context: { [key: string]: any };
-  }) {
-    super(config);
-  }
-
-  public visitFieldDefinition(field: any) {
-    const { resolve = defaultFieldResolver } = field;
-    field.resolve = auth(resolve);
-  }
-}
-
-export class AuthKeyDirective extends SchemaDirectiveVisitor {
-  constructor(config: {
-    name: string;
-    visitedType: VisitableSchemaType;
-    schema: GraphQLSchema;
-    args: {};
-    context: { [key: string]: any };
-  }) {
-    super(config);
-  }
-
-  public visitFieldDefinition(field: any) {
-    const { resolve = defaultFieldResolver } = field;
-    field.resolve = authKey(resolve);
-  }
-}
-
-export class TenantDirective extends SchemaDirectiveVisitor {
-  constructor(config: {
-    name: string;
-    visitedType: VisitableSchemaType;
-    schema: GraphQLSchema;
-    args: {};
-    context: { [key: string]: any };
-  }) {
-    super(config);
-  }
-
-  public visitFieldDefinition(field: any) {
-    const { resolve = defaultFieldResolver } = field;
-    field.resolve = tenant(resolve);
-  }
-}
-
-export class HasRoleDirective extends SchemaDirectiveVisitor {
-  constructor(config: {
-    name: string;
-    args: { [name: string]: any };
-    visitedType: VisitableSchemaType;
-    schema: GraphQLSchema;
-    context: { [key: string]: any };
-  }) {
-    super(config);
-  }
-
-  public visitFieldDefinition(field: any) {
-    const { resolve = defaultFieldResolver } = field;
-    const roles = this.parseAndValidateArgs(this.args);
-    field.resolve = hasRole(roles)(resolve);
-  }
-
-  /**
-   *
-   * validate a potential string or array of values
-   * if an array is provided, cast all values to strings
-   */
-  public parseAndValidateArgs(args: { [name: string]: any }): Array<string> {
-    const keys = Object.keys(args);
-
-    if (keys.length === 1 && keys[0] === 'role') {
-      const role = args[keys[0]];
-      if (typeof role == 'string') {
-        return [role];
-      } else if (Array.isArray(role)) {
-        return role.map((val) => String(val));
-      } else {
-        throw new Error(
-          `invalid hasRole args. role must be a String or an Array of Strings`
-        );
+export function AuthDirective(schema: GraphQLSchema, directiveName: any) {
+  return mapSchema(schema, {
+    [MapperKind.OBJECT_FIELD]: (fieldConfig: any) => {
+      const authDirective = getDirective(schema, fieldConfig, directiveName)?.[0];
+      if (authDirective) {
+        const { resolve = defaultFieldResolver } = fieldConfig;
+        fieldConfig.resolve = auth(resolve);
+        return fieldConfig;
       }
     }
-    throw Error("invalid hasRole args. must contain only a 'role argument");
-  }
+  })
 }
 
-export class HasPermissionDirective extends SchemaDirectiveVisitor {
-  constructor(config: {
-    name: string;
-    args: { [name: string]: any };
-    visitedType: VisitableSchemaType;
-    schema: GraphQLSchema;
-    context: { [key: string]: any };
-  }) {
-    super(config);
-  }
-
-  public visitFieldDefinition(field: any) {
-    const { resolve = defaultFieldResolver } = field;
-    const resources = this.parseAndValidateArgs(this.args);
-    field.resolve = hasPermission(resources)(resolve);
-  }
-
-  /**
-   *
-   * validate a potential string or array of values
-   * if an array is provided, cast all values to strings
-   */
-  public parseAndValidateArgs(args: { [name: string]: any }): Array<string> {
-    const keys = Object.keys(args);
-
-    if (keys.length === 1 && keys[0] === 'resources') {
-      const resources = args[keys[0]];
-      if (typeof resources == 'string') {
-        return [resources];
-      } else if (Array.isArray(resources)) {
-        return resources.map((val) => String(val));
-      } else {
-        throw new Error(
-          `invalid hasPermission args. resources must be a String or an Array of Strings`
-        );
+export function AuthKeyDirective(schema: GraphQLSchema, directiveName: any) {
+  return mapSchema(schema, {
+    [MapperKind.OBJECT_FIELD]: (fieldConfig: any) => {
+      const authKeyDirective = getDirective(schema, fieldConfig, directiveName)?.[0];
+      if (authKeyDirective) {
+        const { resolve = defaultFieldResolver } = fieldConfig;
+        fieldConfig.resolve = authKey(resolve);
+        return fieldConfig;
       }
     }
-    throw Error(
-      "invalid hasPermission args. must contain only a 'resources argument"
-    );
+  })
+}
+
+export function TenantDirective(schema: GraphQLSchema, directiveName: any) {
+  return mapSchema(schema, {
+    [MapperKind.OBJECT_FIELD]: (fieldConfig: any) => {
+      const teanantDirective = getDirective(schema, fieldConfig, directiveName)?.[0];
+      if (teanantDirective) {
+        const { resolve = defaultFieldResolver } = fieldConfig;
+        fieldConfig.resolve = tenant(resolve);
+        return fieldConfig;
+      }
+    }
+  })
+}
+
+export function HasRoleDirective(schema: GraphQLSchema, directiveName: any) {
+  return mapSchema(schema, {
+    [MapperKind.OBJECT_FIELD]: (fieldConfig: any) => {
+      const hasRoleDirective = getDirective(schema, fieldConfig, directiveName)?.[0];
+      if (hasRoleDirective) {
+        const { resolve = defaultFieldResolver } = fieldConfig;
+        const roles = parseAndValidateArgsforhasRole(hasRoleDirective);
+        fieldConfig.resolve = hasRole(roles)(resolve);
+        return fieldConfig;
+      }
+    }
+  })
+}
+
+/**
+ *
+ * validate a potential string or array of values
+ * if an array is provided, cast all values to strings
+ */
+function parseAndValidateArgsforhasRole(args: { [name: string]: any }): Array<string> {
+  const keys = Object.keys(args);
+  if (keys.length === 1 && keys[0] === 'role') {
+    const role = args[keys[0]];
+    if (typeof role == 'string') {
+      return [role];
+    } else if (Array.isArray(role)) {
+      return role.map((val) => String(val));
+    } else {
+      throw new Error(
+        `invalid hasRole args. role must be a String or an Array of Strings`
+      );
+    }
   }
+  throw Error("invalid hasRole args. must contain only a 'role argument");
+}
+
+export function HasPermissionDirective(schema: GraphQLSchema, directiveName: any) {
+  return mapSchema(schema, {
+    [MapperKind.OBJECT_FIELD]: (fieldConfig: any) => {
+      const hasPermissionDirective = getDirective(schema, fieldConfig, directiveName)?.[0];
+      if (hasPermissionDirective) {
+        const { resolve = defaultFieldResolver } = fieldConfig;
+        const resources = parseAndValidateArgsforhasPermission(hasPermissionDirective);
+        fieldConfig.resolve = hasPermission(resources)(resolve);;
+        return fieldConfig;
+      }
+    }
+  })
+}
+
+/**
+  *
+  * validate a potential string or array of values
+  * if an array is provided, cast all values to strings
+  */
+function parseAndValidateArgsforhasPermission(args: { [name: string]: any }): Array<string> {
+  const keys = Object.keys(args);
+
+  if (keys.length === 1 && keys[0] === 'resources') {
+    const resources = args[keys[0]];
+    if (typeof resources == 'string') {
+      return [resources];
+    } else if (Array.isArray(resources)) {
+      return resources.map((val) => String(val));
+    } else {
+      throw new Error(
+        `invalid hasPermission args. resources must be a String or an Array of Strings`
+      );
+    }
+  }
+  throw Error(
+    "invalid hasPermission args. must contain only a 'resources argument"
+  );
 }
